2308b393df46acdc5c0c7634e442f6c5aeab4935
SantosM1281 <santosm@sm-portfolio.com>
2016-09-29 16:28:41 -0400

    Angular practice

--- Angular-site/js/app.js
+++ Angular-site/js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', []);
+ (function(){ // Wrapping the code in a closure is always good practice
+    var app = angular.module('store', []); 
+
+    app.controller('StoreController', function(){  //Notice that controller is attached to our app (inside).
+    	this.product = gem;
+    });
+
+    var gem = {
+	  name: 'Dodecahedron',
+	  price: 2.95,
+	  description: 'eastgfjbb',
+	}
+})();
